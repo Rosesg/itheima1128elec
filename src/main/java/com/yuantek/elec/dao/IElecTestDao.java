c572decfafca065c725f5756619d0343fb2a20f0
Rosesg <mizhe2016@163.com>
2017-08-27 15:22:58 +0800

    接口的变量类型 默认为public static final

--- src/main/java/com/yuantek/elec/dao/IElecTestDao.java
+++ src/main/java/com/yuantek/elec/dao/IElecTestDao.java
@@ -3,7 +3,7 @@ package com.yuantek.elec.dao;
 import com.yuantek.elec.domain.ElecTest;
 
 public interface IElecTestDao extends ICommonDao<ElecTest> {
-    // 就不用在此类中定义重复的方法了！
+    // 就不用在此类中定义重复的方法了！ public static final 是默认的！
     public static final String SERVICE_NAME = "com.yuantek.elec.dao.impl.ElecTestDaoImpl";
 
 }
